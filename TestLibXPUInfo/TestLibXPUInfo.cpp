1f6016fd4dfecc015c430a4a00b900693400064c
Eric Palmer <eric.l.palmer@intel.com>
2024-10-30 16:29:50 -0400

    Fix Mac build (#4)

--- TestLibXPUInfo/TestLibXPUInfo.cpp
+++ TestLibXPUInfo/TestLibXPUInfo.cpp
@@ -169,10 +169,14 @@ int printXPUInfo(int argc, char* argv[])
         if (!testIndividual)
         {
             XI::Timer timer;
+#ifdef _WIN32
             APIType apis = APIType((XI::API_TYPE_DXGI | XI::API_TYPE_SETUPAPI \
                 | XI::API_TYPE_DX11_INTEL_PERF_COUNTER | XI::API_TYPE_IGCL | XI::API_TYPE_OPENCL \
                 | XI::API_TYPE_LEVELZERO \
                 | XI::API_TYPE_DXCORE | XI::API_TYPE_NVML) | API_TYPE_WMI);
+#else
+            APIType apis = XI::API_TYPE_METAL;
+#endif
             apis |= additionalAPIs;
             timer.Start();
             std::cout << "Initializing XPUInfo with APIType = " << apis << "...\n";
