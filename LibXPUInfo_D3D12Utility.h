3abdf2cedb391e18e1e1e274d03308b9798cd019
Eric Palmer <eric.l.palmer@intel.com>
2025-02-04 23:18:37 -0800

    Add copyright and comments

--- LibXPUInfo_D3D12Utility.h
+++ LibXPUInfo_D3D12Utility.h
@@ -1,3 +1,8 @@
+// Copyright (C) 2025 Intel Corporation
+// SPDX-License-Identifier: Apache-2.0
+
+// This header/cpp is intended for use by an application for testing purposes and is not part of LibXPUInfo API.
+
 #pragma once
 #ifdef _WIN32
 #include <wrl.h>
@@ -6,6 +11,7 @@
 
 namespace XI {
 
+// For testing purposes, allocate GPU memory in chunks of 16GB or less.
 bool CreateD3D12DeviceAndAllocateResource(IUnknown* pAdapter, size_t sizeInBytes, std::list<Microsoft::WRL::ComPtr<ID3D12Resource>>& outResources);
 
 } // XI
